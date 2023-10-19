46e3ca81bad76fd12fa881fe187e459d2460bf1b
mrexxis <115736911+mrexxis@users.noreply.github.com>
2023-10-19 20:36:14 +0100

    Primeiros componentes, 19/10/2023

--- app/src/main/java/com/mr/componentes/MainActivity.kt
+++ app/src/main/java/com/mr/componentes/MainActivity.kt
@@ -3,13 +3,22 @@ package com.mr.componentes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.engage.common.datamodel.Image
 import com.mr.componentes.ui.theme.ComponentesTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,12 +38,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    /*Text(
         text = "Hello $name!",
         modifier = modifier
-    )
+    )*/
+    Row{
+        Column{
+            Text(text = "Mário Reis")
+            Text(text = "2023/10/13")
+        }
+        Image(
+            painter = painterResource(id = R.drawable._botao_mais),
+            contentDescription = "Logotipo",
+            modifier = Modifier.size(50.dp).clip(CircleShape)
+        )
+    }
 }
 
 @Preview(showBackground = true)
